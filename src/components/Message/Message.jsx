1b947db7b6b1f02ece2d64bb0b4320edb99a42d9
a_z <azdvd@mail.ru>
2022-10-18 15:10:16 +0300

    lesson 2

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,8 +1,34 @@
 import style from './Message.module.css'
+import { useState } from "react"
 
-export const Message = (props) => {
-    return <form className={style.message}>
-        <h2 style={{backgroundColor: 'gray'}}>Message</h2>
-        <p>Message: { props.text }</p>
-    </form>
-}
+export const Message = ({setMessageList}) => {
+    const [value, setValue] = useState("");
+  
+    return (
+      <div className={style.message}>
+        <input 
+          type="text"
+          value={value}
+          onChange={(ev) => {
+            setValue(ev.target.value);
+          }}
+        />
+  
+        <button
+          onClick={() => {
+            setMessageList((prevstate) => {
+                let count = prevstate.length + 1
+                setTimeout(() => console.log("author-" + count + " wrote: " + value), 1500)             
+
+                return [...prevstate, {
+                    id: count,
+                    text: value,
+                    author: "author-" + (count)
+                }]
+            });            
+          }}>
+          Добавить сообщение
+        </button>
+      </div>
+    );
+  };
